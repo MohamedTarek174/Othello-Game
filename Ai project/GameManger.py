50744e1e3a8667cfac8f7f97d77cd1755289df23
MohamedTarek174 <102221725+MohamedTarek174@users.noreply.github.com>
2024-05-16 02:57:49 +0300

    Update GameManger.py

--- Ai project/GameManger.py
+++ Ai project/GameManger.py
@@ -1,3 +1,11 @@
+# Author : Mohamed Tarek Abdelsattar
+# Date : 05-16-2024
+# Othello is a classic board game played by two players.
+# The game is played on an 8x8 grid board.
+# Each player has disks that are typically black on one side and white on the other.
+# The objective of the game is to have the majority of disks turned to display your color (black or white) by the end of the game.
+# The Game uses Alpha-Beta Method (AI) to Generate the best solution for Meduim & Hard levels
+
 import tkinter as tk
 from Board import *
 from Player import *
